3bef291d1b49eac43a29b451f21bf6d4830c1a3d
Josua Ristiluoma <142972533+Gweedori@users.noreply.github.com>
2026-05-20 15:26:42 +0300

    Flowe task

---
+++ Prefabs/flower_tas/flower_task.gd
@@ -0,0 +1,22 @@
+extends Node2D
+@onready var flower_task: Node2D = $"."
+@onready var taskmanager = flower_task.get_parent()
+@onready var task_area: Area2D = $TaskArea
+
+@onready var bad_meadow: TileMapLayer = $BadMeadow
+@onready var good_meadow: TileMapLayer = $GoodMeadow
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	good_meadow.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func task_done():
+	bad_meadow.hide()
+	good_meadow.show()
