a35ef05e310fbe91899ae31560d8b13a913a0c80
Josua Ristiluoma <142972533+Gweedori@users.noreply.github.com>
2026-05-20 17:06:53 +0300

    Juttu

---
+++ screens/win_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	AudioManager.basic_action.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Menu/main_menu.tscn")
