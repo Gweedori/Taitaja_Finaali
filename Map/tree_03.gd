bcfcdfcec7401a4aa206c4d9641e8215328d364d
Matias Kanervala <153278985+MVP194@users.noreply.github.com>
2026-05-20 13:31:15 +0300

    map start

---
+++ Map/tree_03.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	changeStage(1)
+	print(sprite.region_rect)
+
+#[P: (0.0, 0.0), S: (112.0, 160.0)]
+#[P: (112.0, 0.0), S: (112.0, 160.0)]
+#[P: (112.0, 160.0), S: (112.0, 160.0)]
+
+
+func changeStage(stage):
+	match stage:
+		1:
+			sprite.region_rect.position = Vector2(0.0, 256.0)
+		2:
+			sprite.region_rect.position = Vector2(128.0, 0.0)
+		3:
+			sprite.region_rect.position = Vector2(00.0, 0.0)
